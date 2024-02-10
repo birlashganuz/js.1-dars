c2d271b3183b7fbebdfb81c3d82835ce7dabd153
birlashganuz <birlashganuz@gmail.com>
2024-02-10 18:26:53 +0500

    Kamchiliklari tugatildi.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 var name = prompt('Ismingizni kiriting:');
 console.log('Mening ismim: ' + name);
-var age = prompt('Qaysi yilda tug\'ilgansiz:');
+var age = prompt('Tug\'ilgan yilingizni kiriting:');
 console.log('Men ' + (2024 - age) + ' yoshdaman');
 
 var x = 12;
@@ -19,13 +19,13 @@ var y = 17;
 var a = prompt((x) + '*' + (y) + '=');
 console.log('3-misol: ' + (x) + '*' + (y) + '=' + (x * y) + '. Sizning javobingiz:' + a);
 
-var x = 12;
-var y = 17;
+var x = 48;
+var y = 12;
 var a = prompt((x) + '/' + (y) + '=');
 console.log('4-misol: ' + (x) + '/' + (y) + '=' + (x / y) + '. Sizning javobingiz:' + a);
 
-var x = 12;
-var y = 17;
+var x = 17;
+var y = 3;
 var a = prompt((x) + '%' + (y) + '=');
 console.log('5-misol: ' + (x) + '%' + (y) + '=' + (x % y) + '. Sizning javobingiz:' + a);
 
@@ -34,6 +34,11 @@ var y = 7;
 var a = prompt((x) + '+' + (y) + '*' + (x) + '=');
 console.log('6-misol: ' + (x) + '+' + (y) + '*' + (x) + '=' + (x + y * x) + '. Sizning javobingiz:' + a);
 
+var x = 5;
+var y = 7;
+var a = prompt((x) + '*' + (y) + '-' + (x) + '=');
+console.log('7-misol: ' + (x) + '*' + (y) + '-' + (x) + '=' + (x * y - x) + '. Sizning javobingiz:' + a);
 
-// alert("Natijalarni consoleda ko`rishingiz mumkin.")
+
+alert("Natijalarni consoleda ko`rishingiz mumkin.")
 
